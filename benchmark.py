2d539f3f469dc6d7278c5dd9643c6f35deb34f86
Todd Lipcon <todd@apache.org>
2020-03-04 13:07:42 -0800

    add query support

--- benchmark.py
+++ benchmark.py
@@ -1,34 +1,59 @@
 #!/usr/bin/env python
 
 import logging
+import fnmatch
 import click
 import os
 import subprocess
 
 GOROOT=os.path.join(os.environ.get('GOROOT', os.path.expanduser("~/go")))
 TSBS_GENERATE_DATA=os.path.join(GOROOT, "bin", "tsbs_generate_data")
+TSBS_GENERATE_QUERIES=os.path.join(GOROOT, "bin", "tsbs_generate_queries")
 
 
 SYSTEMS=dict(
   kudu=dict(
-    loader="tsbs_load_influx",
     extra_load_flags=["--do-create-db=0"],
     format="influx",
     url="http://localhost:4242",
   ),
   influx=dict(
-    loader="tsbs_load_influx",
     format="influx",
     url="http://localhost:8086",
   ),
 )
 
+WORKLOADS=[
+  "cpu-max-all-1",
+  "cpu-max-all-8",
+  "double-groupby-1",
+  "double-groupby-5",
+  "double-groupby-all",
+  "high-cpu-1",
+  "high-cpu-all",
+  "single-groupby-1-1-1",
+  "single-groupby-1-1-12",
+  "single-groupby-1-8-1",
+  "single-groupby-5-1-1",
+  "single-groupby-5-1-12",
+  "single-groupby-5-8-1",
+  # NOT SUPPORTED:
+  #"groupby-orderby-limit",
+  # "lastpoint",
+]
 
 
 SCALE=4000
 TS_START="2019-04-01T00:00:00Z"
 TS_END="2019-04-04T00:00:00Z"
 LOAD_WORKERS=8
+COMMON_ARGS=[
+    "--use-case=cpu-only",
+    "--seed=123",
+    "--scale={}".format(SCALE),
+    "--timestamp-start={}".format(TS_START),
+    "--timestamp-end={}".format(TS_END),
+]
 
 @click.group()
 def cli():
@@ -44,13 +69,8 @@ def generate_data(system, out_path):
   tmp_path = "{}.tmp".format(out_path)
   zstd = subprocess.Popen(["zstd", "-f", "-o", tmp_path], stdin=subprocess.PIPE)
   cmd=[TSBS_GENERATE_DATA,
-    "--use-case=cpu-only",
-    "--seed=123",
-    "--scale={}".format(SCALE),
-    "--timestamp-start={}".format(TS_START),
-    "--timestamp-end={}".format(TS_END),
     "--log-interval=10s",
-    "--format={}".format(system)]
+    "--format={}".format(system)] + COMMON_ARGS
   try:
     subprocess.check_call(cmd, stdout=zstd.stdin)
     zstd.stdin.close()
@@ -67,7 +87,7 @@ def tee(path):
   return p.stdin
 
 def load_data(system, input_zst_path):
-  loader = os.path.join(GOROOT, "bin", SYSTEMS[system]['loader'])
+  loader = os.path.join(GOROOT, "bin", 'tsbs_load_{}'.format(SYSTEMS[system]['format']))
   zstd = subprocess.Popen(["zstdcat", input_zst_path], stdout=subprocess.PIPE)
   cmd=[loader,
     "--gzip=0",
@@ -86,6 +106,35 @@ def load(system):
   data_path = "data-{}-scale-{}.txt.zst".format(file_format, SCALE)
   generate_data(file_format, data_path)
   load_data(system, data_path)
+
+def _query_count_multiple(workload):
+  if "single-group" in workload or 'cpu-max' in workload:
+    return 1000
+  return 1
+
+@cli.command(name="run-queries")
+@click.option("--workloads", default="*")
+@click.option("--workers", default=1, type=int)
+@click.argument("system")
+def run_queries(system, workloads, workers):
+  for workload in fnmatch.filter(WORKLOADS, workloads):
+    logging.info("=== Running workload %s with %s workers", workload, workers)
+    gen_queries = subprocess.Popen(
+        [TSBS_GENERATE_QUERIES] + COMMON_ARGS +
+        ["--format={}".format(SYSTEMS[system]['format']),
+         "--query-type={}".format(workload),
+         "--queries={}".format(workers * _query_count_multiple(workload))],
+        stdout=subprocess.PIPE)
+    runner = os.path.join(GOROOT, "bin",
+        "tsbs_run_queries_{}".format(SYSTEMS[system]['format']))
+    subprocess.check_call(
+        [runner,
+         "--workers={}".format(workers),
+         "--urls={}".format(SYSTEMS[system]['url']),
+         "--print-interval=0"],
+        stdin=gen_queries.stdout)
+    gen_queries.communicate()
+    print("\n\n")
 
 if __name__ == "__main__":
   logging.basicConfig(level=logging.INFO)
